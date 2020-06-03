d27f1ad728443d54435f8932d75fb58b4a79cd9e
BuildTools <unconfigured@null.spigotmc.org>
2020-06-03 19:41:19 +0100

    Adjusted UI, Added configurability

--- src/main/java/xyz/probablynotkai/Main.java
+++ src/main/java/xyz/probablynotkai/Main.java
@@ -11,65 +11,9 @@ import java.util.*;
 
 public class Main
 {
-    public static UI ui = new UI();
-
-    public static HashMap<String, String> db = new HashMap<String, String>();
-
     @lombok.SneakyThrows
     public static void main(String[] args) {
-        File dataFile = new File("accounts.txt");
-        if (!dataFile.exists()){
-            dataFile.createNewFile();
-        }
-
-        for (String entry : DataManagement.readFullFile(dataFile)){
-            String[] arr = entry.split(";");
-
-            String username = arr[0];
-            String password = arr[1];
-
-            db.put(username, password);
-        }
-
-        ui.runGUI(dataFile);
-    }
-
-    public static void saveNewEntries(File file){
-        for (Map.Entry<String, String> map : db.entrySet()){
-            String value = map.getKey() + ";" + map.getValue();
-
-            boolean dupeFound = false;
-            for (String entry : DataManagement.readFullFile(file)){
-                if (entry.equals(value)){
-                    dupeFound = true;
-                }
-            }
-
-            if (!dupeFound){
-                DataManagement.writeTo(file, value);
-            }
-        }
-    }
-
-    public static String getRandomEntry(String username, String password){
-        Random random = new Random();
-
-        String[] values = db.values().toArray(new String[0]);
-        String randomSet = values[random.nextInt(values.length)];
-
-        String u = "";
-        String p = "";
-
-        for (Map.Entry<String, String> map : db.entrySet()){
-            String pas = map.getValue();
-            String user = map.getKey();
-
-            if (pas.equals(randomSet)){
-                p = map.getValue();
-                u = map.getKey();
-            }
-        }
-
-        return u + ";" + p;
+        UI ui = new UI();
+        ui.runGUI();
     }
 }
--- src/main/java/xyz/probablynotkai/UI.java
+++ src/main/java/xyz/probablynotkai/UI.java
@@ -23,28 +23,27 @@ import java.util.List;
 
 public class UI
 {
-    public static Instagram4j instagram;
-    public static String username;
-    public static String password;
+    public Instagram4j instagram;
+    public String username;
+    public String password;
 
-    public void runGUI(final File file){
+    public void runGUI(){
         final JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle("K Tool - Login");
 
         final JTextField usernameT = new JTextField();
-        usernameT.setSelectedTextColor(Color.RED);
-        usernameT.setDisabledTextColor(Color.black);
-        usernameT.setBounds(75, 75, 125, 27);
+        usernameT.setFont(Font.getFont(Font.SANS_SERIF));
+        usernameT.setBounds(85, 75, 125, 27);
 
         final JPasswordField passwordT = new JPasswordField();
-        passwordT.setSelectedTextColor(Color.RED);
-        passwordT.setDisabledTextColor(Color.black);
-        passwordT.setBounds(75, 110, 125, 27);
+        passwordT.setBounds(85, 110, 125, 27);
+        passwordT.setFont(Font.getFont(Font.SANS_SERIF));
         passwordT.setEchoChar('*');
 
         final JButton login = new JButton("Login");
-        login.setBounds(85, 150, 100, 40); // X, Y, Width, Height
+        login.setFont(Font.getFont(Font.SANS_SERIF));
+        login.setBounds(95, 150, 100, 40); // X, Y, Width, Height
         login.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if ((username = usernameT.getText()) == null
@@ -61,12 +60,6 @@ public class UI
                         passwordT.setText("");
                         return;
                     } else {
-                        if (!Main.db.containsKey(usernameT.getText())
-                        || !Main.db.containsValue(passwordT.getText())){
-                            Main.db.put(usernameT.getText(), passwordT.getText());
-                        }
-                        Main.saveNewEntries(file);
-
                         runAdminGUI(frame);
                         usernameT.setText("");
                         passwordT.setText("");
@@ -89,7 +82,8 @@ public class UI
         frame.setResizable(false);
     }
 
-    public void runAdminGUI(final JFrame frame){
+    public boolean isSpammingMessage;
+    private void runAdminGUI(final JFrame frame){
         frame.setVisible(false);
 
         final JFrame admin = new JFrame();
@@ -99,25 +93,30 @@ public class UI
         // SPAM MODE
         final JTextField spamTarget = new JTextField();
         spamTarget.setBounds(200, 123, 100, 27);
+        spamTarget.setFont(Font.getFont(Font.SANS_SERIF));
         spamTarget.setToolTipText("Target");
 
         final JTextField spamMessage = new JTextField();
         spamMessage.setBounds(200, 150, 100, 27);
+        spamMessage.setFont(Font.getFont(Font.SANS_SERIF));
         spamMessage.setToolTipText("Message Text");
 
         final JTextField spamNumber = new JTextField();
         spamNumber.setBounds(200, 177, 100, 27);
         spamNumber.setToolTipText("Number Of Messages");
         spamNumber.setText("4");
+        spamNumber.setFont(Font.getFont(Font.SANS_SERIF));
 
         // COMMENT MODE
         final JTextField commentTarget = new JTextField();
         commentTarget.setBounds(200, 123, 100, 27);
+        commentTarget.setFont(Font.getFont(Font.SANS_SERIF));
         commentTarget.setToolTipText("Target");
         commentTarget.setVisible(false);
 
         final JTextField commentMessage = new JTextField();
         commentMessage.setBounds(200, 150, 100, 27);
+        commentMessage.setFont(Font.getFont(Font.SANS_SERIF));
         commentMessage.setToolTipText("Comment Text");
         commentMessage.setVisible(false);
 
@@ -125,18 +124,22 @@ public class UI
         commentNumber.setBounds(200, 177, 100, 27);
         commentNumber.setToolTipText("Number Of Comments");
         commentNumber.setText("4");
+        commentNumber.setFont(Font.getFont(Font.SANS_SERIF));
         commentNumber.setVisible(false);
 
         final JButton commentMode = new JButton("Comment Mode");
         commentMode.setEnabled(true);
         commentMode.setBounds(125, 10, 250, 40);
+        commentMode.setFont(Font.getFont(Font.SANS_SERIF));
 
         final JButton spamMode = new JButton("Spam Message Mode");
+        spamMode.setFont(Font.getFont(Font.SANS_SERIF));
         spamMode.setEnabled(false);
         spamMode.setBounds(125, 60, 250, 40);
 
         JButton logout = new JButton("Logout");
-        logout.setBounds(125, 210, 250, 40);
+        logout.setFont(Font.getFont(Font.SANS_SERIF));
+        logout.setBounds(125, 400, 250, 40);
         logout.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 admin.setVisible(false);
@@ -145,6 +148,7 @@ public class UI
         });
 
         final JButton sendSpam = new JButton("Send Spam Message");
+        sendSpam.setFont(Font.getFont(Font.SANS_SERIF));
         sendSpam.setBounds(125, 257, 250, 40);
         sendSpam.addActionListener(new ActionListener() {
             @SneakyThrows
@@ -166,55 +170,53 @@ public class UI
                 InstagramSearchUsernameResult user = instagram.sendRequest(new InstagramSearchUsernameRequest(spamTarget.getText()));
                 recipients.add(String.valueOf(user.getUser().getPk()));
 
-                for (int i = 0; i < spam; i++){
-                    if (!instagram.isLoggedIn()){
-                        instagram = Instagram4j.builder().username(username).password(password).build();
-                    }
+                if (isSpammingMessage){
+                    for (int i = 0; i < spam; i++) {
+                        if (!instagram.isLoggedIn()) {
+                            instagram = Instagram4j.builder().username(username).password(password).build();
+                        }
 
-                    if (instagram.sendRequest(InstagramDirectShareRequest.builder().recipients(recipients).shareType(InstagramDirectShareRequest.ShareType.MESSAGE).message(String.valueOf(spamMessage.getText())).build()).getStatus().equalsIgnoreCase("fail")){
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println("CHANGING ACCOUNT DUE TO STATUS FAILURE");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.println(" ");
-
-                        String[] entry = Main.getRandomEntry(username, password).split(";");
-
-                        username = entry[0];
-                        password = entry[1];
-
-                        instagram = Instagram4j.builder().username(username).password(password).build();
-                        instagram.setup();
-
-                        try{
-                            instagram.login();
-                            System.out.println("Account successfully switched...");
-                        } catch (Exception e1){
-                            System.out.println("Error while logging into new account...");
+                        if (instagram.sendRequest(InstagramDirectShareRequest.builder().recipients(recipients).shareType(InstagramDirectShareRequest.ShareType.MESSAGE).message(String.valueOf(spamMessage.getText())).build()).getStatus().equalsIgnoreCase("fail")) {
+                            System.out.println(" ");
+                            System.out.println("Sending message failed, retrying in: 3 minutes.");
+                            System.out.println(" ");
+                            Thread.sleep(180000);
                         }
                     }
+                } else {
+                    instagram.sendRequest(InstagramDirectShareRequest.builder().shareType(InstagramDirectShareRequest.ShareType.MESSAGE).recipients(recipients).message(spamMessage.getText()).build());
+                }
+            }
+        });
+
+        final JButton spamMessageToggle = new JButton("Toggle Spam: ON");
+        spamMessageToggle.setFont(Font.getFont(Font.SANS_SERIF));
+        isSpammingMessage = true;
+        spamMessageToggle.setBounds(125, 210, 250, 40);
+        spamMessageToggle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isSpammingMessage){
+                    isSpammingMessage = false;
+                    spamMessageToggle.setText("Toggle Spam: OFF");
+                    spamNumber.setVisible(false);
+                    spamMessageToggle.setBounds(125, 185, 250, 40);
+                    sendSpam.setBounds(125, 230, 250, 40);
+                    sendSpam.setText("Send Message");
+                } else {
+                    isSpammingMessage = true;
+                    spamMessageToggle.setText("Toggle Spam: ON");
+                    spamMessageToggle.setBounds(125, 210, 250, 40);
+                    sendSpam.setBounds(125, 257, 250, 40);
+                    spamNumber.setVisible(true);
+                    sendSpam.setText("Send Spam Message");
                 }
             }
         });
 
         final JButton sendComment = new JButton("Send Comments");
         sendComment.setBounds(125, 257, 250, 40);
+        sendComment.setFont(Font.getFont(Font.SANS_SERIF));
         sendComment.setVisible(false);
         sendComment.addActionListener(new ActionListener() {
             @SneakyThrows
@@ -243,41 +245,9 @@ public class UI
 
                         if (instagram.sendRequest(new InstagramPostCommentRequest(item.getPk(), commentMessage.getText())).getStatus().equalsIgnoreCase("fail")){
                             System.out.println(" ");
+                            System.out.println("Sending message failed, retrying in: 3 minutes.");
                             System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println("CHANGING ACCOUNT DUE TO STATUS FAILURE");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-                            System.out.println(" ");
-
-                            String[] entry = Main.getRandomEntry(username, password).split(";");
-
-                            username = entry[0];
-                            password = entry[1];
-
-                            instagram = Instagram4j.builder().username(username).password(password).build();
-                            instagram.setup();
-
-                            try{
-                                instagram.login();
-                                System.out.println("Account successfully switched...");
-                            } catch (Exception e1){
-                                System.out.println("Error while logging into new account...");
-                            }
+                            Thread.sleep(180000);
                         }
                     }
                 }
@@ -292,6 +262,7 @@ public class UI
                 spamTarget.setVisible(false);
                 spamMessage.setVisible(false);
                 spamNumber.setVisible(false);
+                spamMessageToggle.setVisible(false);
                 commentNumber.setVisible(true);
                 commentTarget.setVisible(true);
                 sendComment.setVisible(true);
@@ -307,10 +278,15 @@ public class UI
                 spamTarget.setVisible(true);
                 spamMessage.setVisible(true);
                 spamNumber.setVisible(true);
+                spamMessageToggle.setVisible(true);
                 commentNumber.setVisible(false);
                 commentTarget.setVisible(false);
                 sendComment.setVisible(false);
                 commentMessage.setVisible(false);
+
+                spamMessageToggle.setBounds(125, 210, 250, 40);
+                sendSpam.setBounds(125, 257, 250, 40);
+                spamNumber.setVisible(true);
             }
         });
 
@@ -321,6 +297,7 @@ public class UI
         admin.add(spamTarget);
         admin.add(spamNumber);
         admin.add(spamMessage);
+        admin.add(spamMessageToggle);
 
         admin.add(commentMode);
         admin.add(commentNumber);
